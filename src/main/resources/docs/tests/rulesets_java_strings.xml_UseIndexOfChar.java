df258988b5dd62a583b6952e53732ed501da4821
Johann Egger <johann@codacy.com>
2015-08-25 21:55:53 +0300

    still didn't test... now it works (FT-257)

--- src/main/resources/docs/tests/rulesets_java_strings.xml_UseIndexOfChar.java
+++ src/main/resources/docs/tests/rulesets_java_strings.xml_UseIndexOfChar.java
@@ -1,18 +1,21 @@
 //#Patterns: rulesets_java_strings.xml_UseIndexOfChar
 
 public class Foo {
+
     public void bar() {
 
         String s = "hello world";
 
         // avoid this
         //#Warn: rulesets_java_strings.xml_UseIndexOfChar
-        if (s.indexOf("d") {}
+        if (s.indexOf("d") == 1) {
+
+        }
 
         // instead do this
-        if (s.indexOf('d') {}
+        if (s.indexOf('d') == 1) {
 
-
+        }
     }
 }
 

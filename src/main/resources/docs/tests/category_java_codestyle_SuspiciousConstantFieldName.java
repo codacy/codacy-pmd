853cdd52996d2463ad30c05f1894c05bf71223b0
Fernando Chovich <fernando@codacy.com>
2019-05-16 18:45:05 +0100

    feature: Run the tests correctly by removing deprecated rules FT-6914

--- src/main/resources/docs/tests/category_java_codestyle_SuspiciousConstantFieldName.java
+++
@@ -1,8 +0,0 @@
-//#Patterns: category_java_codestyle_SuspiciousConstantFieldName
-public class Foo {
-	//#Info: category_java_codestyle_SuspiciousConstantFieldName
-    double PI = 3.16;
-
-    final double MAX = 256;
-
-}

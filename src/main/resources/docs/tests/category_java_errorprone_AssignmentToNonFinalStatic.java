eff0832c7cb47a1aef5dd07075fb0eebb61907af
Fernando Chovich <fernando@codacy.com>
2019-05-16 19:13:58 +0100

    feature: Run the tests correctly by fixing validation of category_java_errorprone_AssignmentToNonFinalStatic rule FT-6914

--- src/main/resources/docs/tests/category_java_errorprone_AssignmentToNonFinalStatic.java
+++ src/main/resources/docs/tests/category_java_errorprone_AssignmentToNonFinalStatic.java
@@ -1,9 +1,9 @@
 //#Patterns: category_java_errorprone_AssignmentToNonFinalStatic
 
 public class StaticField {
-    //#Warn: category_java_errorprone_AssignmentToNonFinalStatic
    static int x;
    public FinalFields(int y) {
+    //#Warn: category_java_errorprone_AssignmentToNonFinalStatic
     x = y; // unsafe
    }
 }

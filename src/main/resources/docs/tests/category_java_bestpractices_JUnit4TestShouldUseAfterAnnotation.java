fc85e36f3ffa663a67b70ed8f394ce2c7c803ff9
David Marinho <david.marinho@codacy.com>
2024-09-13 09:30:28 +0100

    fix tests

--- src/main/resources/docs/tests/category_java_bestpractices_JUnit4TestShouldUseAfterAnnotation.java
+++
@@ -1,9 +0,0 @@
-//#Patterns: category_java_bestpractices_JUnit4TestShouldUseAfterAnnotation
-
-public class MyTest {
-    //#Warn: category_java_bestpractices_JUnit4TestShouldUseAfterAnnotation
-    public void tearDown() {
-        bad();
-    }
-}
-//public class MyTest2 { @After public void tearDown() { good(); } }
